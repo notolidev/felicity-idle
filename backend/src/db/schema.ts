fc4302866931db82982aa1bac6519f198857f444
Oli <palmbusiness12@gmail.com>
2026-06-02 18:18:50 +0100

    Schema defined for v1 (no indexes)

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,6 +1,33 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, real, text, varchar } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    username: varchar({ length: 255 }).notNull(),
+export const players = pgTable("players", {
+    user_id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    username: varchar({ length: 20 }).notNull(),
+    password_hash: text().notNull(),
+});
+
+export const player_skills = pgTable("player_skills", {
+    user_id: integer().references(() => players.user_id),
+    skill_type: text().notNull(),
+    xp: real().notNull(),
+});
+
+export const player_collections = pgTable("player_collections", {
+    user_id: integer().references(() => players.user_id),
+    item_type: text().notNull(),
+    amount: integer().notNull(),
+});
+
+export const player_economy = pgTable("player_economy", {
+    user_id: integer().references(() => players.user_id),
+    coins_purse: integer().notNull(),
+    coins_bank: integer().notNull(),
+});
+
+export const player_minions = pgTable("player_minions", {
+    user_id: integer().references(() => players.user_id),
+    minion_type: text().notNull(),
+    tier: integer().notNull(),
+    amount: integer().notNull(),
+    last_collected_at: text().notNull(),
 });
