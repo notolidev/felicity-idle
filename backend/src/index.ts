e2638ccab1166eb817ca61f3b80dc28b4f530ede
Oli <palmbusiness12@gmail.com>
2026-06-03 09:59:27 +0100

    Added CORS for express server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import cors from "cors";
+
 const app = express();
+app.use(cors());
 const port = 3000;
 
 app.get("/", (req: express.Request, res: express.Response) => {
