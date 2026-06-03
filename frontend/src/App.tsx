057a2d24e56ab41ac64814572cf3d97200f0f9cd
Oli <palmbusiness12@gmail.com>
2026-06-03 10:07:01 +0100

    Basic form switching

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,55 @@
 "use client";
 
+import { useState } from "react";
 import "./app.css";
 
 export default function App() {
+    const [form, setForm] = useState("signIn");
+
     return (
         <div className="auth-container">
-            {/* Sign In */}
-            <div className="auth-card">
-                <h2>Sign In</h2>
-                <label>
-                    Username
-                    <input type="text" placeholder="Username" />
-                </label>
-                <label>
-                    Password
-                    <input type="password" placeholder="Password" />
-                </label>
-                <button type="button">Sign In</button>
-            </div>
-
-            {/* Sign Up */}
-            <div className="auth-card">
-                <h2>Sign Up</h2>
-                <label>
-                    Username
-                    <input type="text" placeholder="Username" />
-                </label>
-                <label>
-                    Password
-                    <input type="password" placeholder="Password" />
-                </label>
-                <button type="button">Sign Up</button>
-            </div>
+            <button
+                onClick={() => {
+                    if (form === "signUp") {
+                        setForm("signIn");
+                    } else if (form === "signIn") {
+                        setForm("signUp");
+                    }
+                }}
+            >
+                Change form
+            </button>
+            {form == "signIn" ? (
+                /* Sign In */
+                <div className="auth-card">
+                    <h2>Sign In</h2>
+                    <label>
+                        Username
+                        <input type="text" placeholder="Username" />
+                    </label>
+                    <label>
+                        Password
+                        <input type="password" placeholder="Password" />
+                    </label>
+                    <button type="button">Sign In</button>
+                </div>
+            ) : form == "signUp" ? (
+                /* Sign Up */
+                <div className="auth-card">
+                    <h2>Sign Up</h2>
+                    <label>
+                        Username
+                        <input type="text" placeholder="Username" />
+                    </label>
+                    <label>
+                        Password
+                        <input type="password" placeholder="Password" />
+                    </label>
+                    <button type="button">Sign Up</button>
+                </div>
+            ) : (
+                <h1>Wrong!</h1>
+            )}
         </div>
     );
 }
